02d6aa33246064272eee4170355e6d67afc678f2
Rakhshan khan <rakshaankhan1@gmail.com>
2024-03-04 12:31:01 +0530

    add authoritzation functioning

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import './App.css'
 
 function App() {
-  
+  console.log(import.meta.env.VITE_APPWRITE_URL)
 
-  return (
-    <h1>Blog with appwrite</h1>
-  )
+  return <h1>Blog with appwrite</h1>
 }
 
 export default App
